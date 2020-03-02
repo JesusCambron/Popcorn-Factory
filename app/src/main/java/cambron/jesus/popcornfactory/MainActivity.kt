21d558b72500a5f68b4aea64078f18aaea31c939
JesusCambron <manuel.cam98@gmail.com>
2020-03-01 21:39:53 -0700

    Primer Commit

---
+++ app/src/main/java/cambron/jesus/popcornfactory/MainActivity.kt
@@ -0,0 +1,31 @@
+package cambron.jesus.popcornfactory
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var intento: Intent = Intent(this, CatalogActivity::class.java)
+
+        val boton: Button = findViewById(R.id.boton) as Button
+
+        boton.setOnClickListener {
+            startActivity(intento)
+        }
+
+
+
+
+    }
+
+
+
+
+}
